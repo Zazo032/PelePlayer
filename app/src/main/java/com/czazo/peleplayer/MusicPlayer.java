7aeec58f38881e0a6a11b28401562a7055d41357
jemarhe <jemarhe2@gmail.com>
2018-05-08 11:42:43 +0200

    MusicPlayer IMPROVED

--- app/src/main/java/com/czazo/peleplayer/MusicPlayer.java
+++ app/src/main/java/com/czazo/peleplayer/MusicPlayer.java
@@ -1,13 +1,25 @@
 package com.czazo.peleplayer;
 
+import android.media.AudioAttributes;
 import android.media.MediaPlayer;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MusicPlayer extends MediaPlayer {
     private MusicPlayer instance;
-    private Song currentSong;
+    private int posCurrentSong;
+    private List<Song> songs;
 
     private MusicPlayer() {
         super();
+        this.setAudioAttributes( new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build()
+        );
     }
 
     public MusicPlayer getInstance() {
@@ -17,13 +29,37 @@ public class MusicPlayer extends MediaPlayer {
         return instance;
     }
 
-    public Song getCurrentSong() {
-        return currentSong;
+    public void play(int pos){
+        this.reset();
+        Song songToPlay = songs.get(pos);
+        try {
+            this.setDataSource(songToPlay.getFilePath());
+            this.prepare();
+            this.posCurrentSong = pos;
+            this.start();
+        } catch (IOException e) {
+            Log.e("MusicPlayer", "No se ha encontrado el archivo en la ruta de datos");
+            play(pos++);
+        }
     }
 
-    public void setCurrentSong(Song currentSong) {
-        this.currentSong = currentSong;
+    public void playNext() {
+        play(posCurrentSong++);
     }
 
+    public void playPrevious() {
+        play(posCurrentSong--);
+    }
 
+    public int getPosCurrentSong() {
+        return posCurrentSong;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
 }
